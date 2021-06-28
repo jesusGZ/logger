f1ff2faac072f669b062a3a8b26055823027bac7
jesusGZ <j.gr.zurita@gmail.com>
2021-06-28 11:27:31 -0500

    uso de transportes

--- logger.js
+++ logger.js
@@ -1,21 +1,24 @@
 const winston = require('winston');
-
 const logConfiguration = {
-    'transports': [
-        new winston.transports.Console()
+    transports: [
+        new winston.transports.Console({
+            level: 'warn'
+        }),
+        new winston.transports.File({
+            level: 'error',
+            // Create the log directory if it does not exist
+            filename: 'logs/example.log'
+        })
     ]
 };
 
 const logger = winston.createLogger(logConfiguration);
 
-// Log a message
-logger.log({
-    // Message to be logged
-        message: 'Hello, Winston!',
-    
-    // Level of the message logging
-        level: 'info'
-    });
-    // Log a message
-    logger.info('Hello, Winston!');
-    
+// Log some messages
+logger.error("Hello, Winston logger, the first error!");
+logger.warn("Hello, Winston logger, the first warning!");
+logger.warn("Hello, Winston logger, the second warning!");
+logger.error("Hello, Winston logger, the second error!");
+logger.info("Hello, Winston logger, some info!");
+logger.debug("Hello, Winston logger, a debug!");
+
